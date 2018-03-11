7d960599c221fbfe9b850409660dd74f5b1fa129
Will Tekulve <tekulve.will@gmail.com>
2018-03-11 17:46:50 -0400

    Add project files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Red-Lavalink',
+    version='0.0.1',
+    packages=['lavalink',],
+    url='https://github.com/Cog-Creators/Red-Lavalink',
+    license='GPLv3',
+    author='tekulvw',
+    description='Lavalink client library for Red-DiscordBot'
+)
