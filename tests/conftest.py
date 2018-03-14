defae66aec2ba7062085713d530c1ac1ee526368
Will Tekulve <tekulve.will@gmail.com>
2018-03-13 20:54:41 -0400

    Some real tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,11 @@
+from collections import namedtuple
+
 import pytest
 import asyncio
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
+
+from discord.gateway import DiscordWebSocket
+from discord.ext.commands import AutoShardedBot
 
 import lavalink.node
 
@@ -13,7 +18,10 @@ class ProxyWebSocket:
         self.emit_data = '{}'
 
         self.recv = MagicMock(wraps=self._recv)
-        self.send = MagicMock()
+        self.send = MagicMock(wraps=self._send)
+
+    async def _send(self, data):
+        pass
 
     async def _recv(self):
         await self.EMIT.wait()
@@ -28,29 +36,94 @@ class ProxyWebSocket:
         self.open = False
 
 
-@pytest.fixture()
-def unconnected_node():
-    old_connect = lavalink.node.websockets.connect
-
-    async def connect(*args, **kwargs):
-        return ProxyWebSocket()
-
-    lavalink.node.websockets.connect = MagicMock(wraps=connect)
-    loop = asyncio.get_event_loop()
-    yield lavalink.node.Node(
-        _loop=loop,
-        event_handler=MagicMock(),
-        voice_ws_func=MagicMock(),
-        host='localhost',
-        password='password',
-        port=2333,
-        user_id=123456,
-        num_shards=1
-    )
-    lavalink.node.websockets.connect = old_connect
+@pytest.fixture
+def user():
+    User = namedtuple('User', 'id')
+    return User(1234567890)
 
 
 @pytest.fixture
-async def node(unconnected_node):
-    await unconnected_node.connect()
-    return unconnected_node
+def guild():
+    Guild = namedtuple('Guild', 'id')
+    return Guild(987654321)
+
+
+@pytest.fixture()
+def voice_channel(guild):
+    VoiceChannel = namedtuple('VoiceChannel', 'id guild')
+    return VoiceChannel(9999999999, guild)
+
+
+@pytest.fixture
+def bot(event_loop, user, voice_channel):
+    async def voice_state(guild_id=None, channel_id=None):
+        pass
+
+    voice_websocket = MagicMock(spec=DiscordWebSocket)
+    voice_websocket.voice_state = MagicMock(wraps=voice_state)
+
+    conn = MagicMock()
+    conn._get_websocket = lambda guild_id: voice_websocket
+
+    bot_ = MagicMock(spec=AutoShardedBot)
+    bot_.loop = event_loop
+    bot_._connection = conn
+    bot_.user = user
+    bot_.get_channel = lambda channel_id: voice_channel
+    bot_.shard_count = 1
+
+    return bot_
+
+
+@pytest.fixture(autouse=True)
+def patch_node():
+    async def connect(*args, **kwargs):
+        return ProxyWebSocket()
+
+    async def send(data):
+        pass
+
+    websockets_patch = patch(
+        'websockets.connect',
+        new=MagicMock(wraps=connect)
+    )
+    node_patch = patch.multiple(
+        'lavalink.node.Node',
+        send=MagicMock(wraps=send)
+    )
+    websockets_patch.start()
+    node_patch.start()
+    yield
+    node_patch.stop()
+    websockets_patch.stop()
+
+
+@pytest.fixture
+async def node(bot):
+    node_ = lavalink.node.Node(
+        _loop=bot.loop,
+        event_handler=MagicMock(),
+        voice_ws_func=bot._connection._get_websocket,
+        host='localhost',
+        password='password',
+        port=2333,
+        user_id=bot.user.id,
+        num_shards=bot.shard_count
+    )
+
+    # node_.send = MagicMock(wraps=send)
+
+    await node_.connect()
+    yield node_
+
+    try:
+        await node_.disconnect()
+    except KeyError:
+        pass
+
+
+@pytest.fixture()
+async def initialize_lavalink(bot):
+    await lavalink.initialize(bot, 'localhost', 'password', 2332, 2333)
+    yield
+    await lavalink.close()
