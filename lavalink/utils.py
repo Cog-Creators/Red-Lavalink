82c9389031802d2b88f54332135bc6d49778a2a6
jack1142 <6032823+jack1142@users.noreply.github.com>
2022-03-02 20:49:34 +0100

    Reformat with Black 22.1.0

--- lavalink/utils.py
+++ lavalink/utils.py
@@ -1,5 +1,5 @@
 def format_time(time):
-    """ Formats the given time into HH:MM:SS """
+    """Formats the given time into HH:MM:SS"""
     h, r = divmod(time / 1000, 3600)
     m, s = divmod(r, 60)
 
