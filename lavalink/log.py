c6df48bc20b9e85370ea61d1b9b4aa03ce3115b8
Draper <27962761+Drapersniper@users.noreply.github.com>
2022-03-18 20:26:18 +0000

    add Red-Commons as a dep

--- lavalink/log.py
+++ lavalink/log.py
@@ -1,39 +1,16 @@
 import logging
+from red_commons.logging import maybe_update_logger_class, getLogger
 
 
-def _update_logger_class():
+maybe_update_logger_class()
 
-    VERBOSE = logging.DEBUG - 3
-    TRACE = logging.DEBUG - 5
-
-    class RedTraceLogger(logging.getLoggerClass()):
-        def __init__(self, name, level=logging.NOTSET):
-            super().__init__(name, level)
-
-            logging.addLevelName(VERBOSE, "VERBOSE")
-            logging.addLevelName(TRACE, "TRACE")
-
-        def verbose(self, msg, *args, **kwargs):
-            if self.isEnabledFor(VERBOSE):
-                self._log(VERBOSE, msg, args, **kwargs)
-
-        def trace(self, msg, *args, **kwargs):
-            if self.isEnabledFor(TRACE):
-                self._log(TRACE, msg, args, **kwargs)
-
-    if logging.getLoggerClass().__name__ != "RedTraceLogger":
-        logging.setLoggerClass(RedTraceLogger)
-
-
-_update_logger_class()
-
-log = logging.getLogger("red.core.RLL")
-socket_log = logging.getLogger("red.core.RLL.socket")
+log = getLogger("red.core.RLL")
+socket_log = getLogger("red.core.RLL.socket")
 socket_log.setLevel(logging.INFO)
 
-ws_discord_log = logging.getLogger("red.Audio.WS.discord")
-ws_ll_log = logging.getLogger("red.Audio.WS.LLNode")
-ws_rll_log = logging.getLogger("red.Audio.WS.RLL")
+ws_discord_log = getLogger("red.Audio.WS.discord")
+ws_ll_log = getLogger("red.Audio.WS.LLNode")
+ws_rll_log = getLogger("red.Audio.WS.RLL")
 
 
 def set_logging_level(level=logging.INFO):
